b879c486e796cc3608b70088643fd729c4c0e203
MarquanB21 <mbowman@captechconsulting.com>
2021-11-12 10:24:48 -0500

    finished product

--- main.js
+++ main.js
@@ -1,36 +1,49 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+//goes with silly-gen.html
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+  return array[Math.floor(Math.random()*array.length)];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.;
+// 2. RAW TEXT STRINGS
 
-let insertX = "Willy the Goblin, Big Daddy, Father Christmas";
+var storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var insertX =['Willy the Goblin','Big Daddy','Father Christmas'];
+var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-let insertY = "the soup kitchen, Disneyland, the White House";
 
-let insertZ = "spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away";
-
-
-randomize.addEventListener('click', result);
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 function result() {
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(/:insertx:/g, xItem).replace(/:inserty:/g, yItem).replace(/:insertz:/g, zItem);
 
   if(customName.value !== '') {
-    let name = customName.value;
-
+    var name = customName.value;
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    console.log(name);
+    newStory = newStory.replace(/Bob/g, name);
   }
-
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
+    var weight = Math.round(300 * 0.071429) + ' stone';
+    newStory = newStory.replace('300 pounds', weight);
+
+    var temperature =  Math.round((94 - 32) * .5556) + ' centigrade' ;
+    newStory = newStory.replace('94 farenheit', temperature);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
+
+randomize.addEventListener('click', result);
